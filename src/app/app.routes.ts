b0802b262ad250e99d4fd0932932eca676d9f654
Nils <niehoelzz@gmail.com>
2024-05-19 19:04:21 +0200

    removed home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Route, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { ToursComponent } from './pages/tours/tours.component';
 import { MissionComponent } from './pages/mission/mission.component';
 import { RocketsComponent } from './pages/rockets/rockets.component';
 
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
     { path: 'tours', component: ToursComponent },
     { path: 'mission', component: MissionComponent },
     { path: 'rockets', component: RocketsComponent },
-    { path: '**', redirectTo: 'home' }
+    { path: '**', redirectTo: 'mission' }
 ];
